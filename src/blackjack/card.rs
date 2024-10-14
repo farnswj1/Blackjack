00b6329d514b599b8a39ebefeb74c5e6172b2cce
Justin Farnsworth <fjj12697@gmail.com>
2024-10-14 16:37:51 -0400

    simplify match statement cases

--- src/blackjack/card.rs
+++ src/blackjack/card.rs
@@ -18,10 +18,7 @@ impl Card {
             "Seven" => 7,
             "Eight" => 8,
             "Nine" => 9,
-            "Ten" => 10,
-            "Jack" => 10,
-            "Queen" => 10,
-            "King" => 10,
+            "Ten" | "Jack" | "Queen" | "King" => 10,
             "Ace" => 11,
             _ => 0
         }
